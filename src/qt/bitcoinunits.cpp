429db7676ab0cb77adc61fe7f18cdb9f11d67bb2
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2014-02-04 16:30:57 -0600

    Updated qt client english translation to Tatcoin/TAT

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("μBTC");
+    case BTC: return QString("TAT");
+    case mBTC: return QString("mTAT");
+    case uBTC: return QString::fromUtf8("μTAT");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Bitcoins");
-    case mBTC: return QString("Milli-Bitcoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Bitcoins (1 / 1,000,000)");
+    case BTC: return QString("Tatcoins");
+    case mBTC: return QString("Milli-Tatcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Tatcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
